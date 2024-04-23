bdc6960b4b54eb6b4ca94cf15873ba1806dd7c38
Luis Giraldo <luisggon@protonmail.com>
2024-04-23 15:30:12 +0200

    added docstring a clearer syntaxis

--- project_euler/euler_01.py
+++ project_euler/euler_01.py
@@ -22,28 +22,75 @@ For each test case, print an integer that denotes the sum of all the multiples o
 
 # auxiliary functions
 
-def sum_arith_prog(n:int, d:int)->int:
-    
-    return ((n+1)*(2*0 + n*d))//2
+def sum_arith_prog(a_0:int, n:int, d:int)->int:
 
-def find_n(d:int, N:int)->int:
+    """
+    Computes the sum of an arithmetic progression.
+
+    Parameters
+    ----------
+    a0: int
+        First term of the arithmetic progression
+    n: int
+        Number of terms from the progression to be added
+    d: int
+        difference between the terms of the arithmetic progression 
     
-    if N%d == 0:
-        return N//d-1
+    Return
+    ------
+    int
+    """
+
+    result = n*(a_0 + a_0 + (n-1)*d)//2
+    
+    return result
+
+def find_last_term(upper_bound:int, number:int)->int:
+
+    """
+    Computes how many times can a number be added without surpasing an upper bound
+
+    Parameters:
+    -----------
+    upper_bound: int
+        upper bound for the number of times that we add number to itself
+    number: int
+        number to add to itself
+    
+    Returns
+    -------
+    int
+    """
+    
+    if upper_bound%number == 0:
+        return upper_bound//number-1
     else:
-        return N//d
+        return upper_bound//number
 
 # solution
 
-def euler_project_1(N)->int:
+def euler_project_1(N:int)->int:
 
-    n_3 = find_n(d=3, N=10)
-    mult_3 = sum_arith_prog(n=n_3, d=3)
+    """
+    Compute the sum of all multiples of 3 or 5 below N
 
-    n_5 = find_n(d=5, N=10)
-    mult_5 = sum_arith_prog(n=n_5, d=5)
+    Parameter
+    ---------
+    N: int
+        upper bound for the multiples of 3 or 5 considered
+
+    Returns
+    -------
+    int 
+    """
+
+    n_3 = find_last_term(upper_bound=N, number=3)
+    mult_3 = sum_arith_prog(a_0=3, n=n_3, d=3)
+
+    n_5 = find_last_term(upper_bound=N, number=5)
+    mult_5 = sum_arith_prog(a_0=5, n=n_5, d=5)
     
-    n_15 = find_n(d=15, N=10)
-    mult_15 = sum_arith_prog(n=n_15, d=15)
+    n_15 = find_last_term(upper_bound=N, number=15)
+    mult_15 = sum_arith_prog(a_0=15, n=n_15, d=15)
 
     return mult_3 + mult_5 - mult_15
