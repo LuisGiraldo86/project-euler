7f30069f47187a7895763c9149c638741e1b6a63
Luis Giraldo <luisggon@protonmail.com>
2024-04-24 15:55:54 +0200

    simpler version

--- project_euler/euler_08.py
+++ project_euler/euler_08.py
@@ -22,20 +22,51 @@ Print the required answer for each test case.
 
 # solution
 
-def list_product(int_str):
+def list_product(int_str:list)->int:
+
+    """
+    Computes the product of the elements of a list.
+
+    Parameter
+    ---------
+    int_str: list
+        list whose elements are integers but formatted as strings.
+
+    Return
+    ------
+    int
+    """
 
     int_lst = [int(num) for num in int_str]
     prod = 1
-    for number in int_lst:
-        if number == 0:
-            return 0
-        else:
-            prod = prod*number
-    return prod
 
-def largest_product(num, K, N):
+    if 0 in int_lst: return 0
+    else:
+        for number in int_lst:
+            prod = prod*number
+        return prod
+
+def largest_product(num:int, K:int, N:int)->int:
+
+    """
+    Find the greatest product of K consecutive digits in the N digit number num.
+
+    Parameters
+    ----------
+    num: int
+        the number of N digits
+    K: int
+        number of consecutive digits
+    N: int
+        length of number num
+
+    Return
+    ------
+    int
+    """
 
     str_num = str(num)
     lst_num = [str_num[i:i+K] for i in range(0,N-K)]
     lst_prod = [list_product(number) for number in lst_num]
+    
     return max(lst_prod)
