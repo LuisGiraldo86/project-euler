59ff1579764090c49e256fbe3dd2bc0680c053c4
Luis Giraldo <luisggon@protonmail.com>
2024-04-24 13:14:23 +0200

    docstrins and typehints

--- project_euler/euler_04.py
+++ project_euler/euler_04.py
@@ -15,7 +15,7 @@ First line contains T that denotes the number of test cases. This is followed by
 Constraints
 -----------
 1 <= T <= 10
-10<= N <= 10^12
+101101 < N < 10^12
 
 Output Format
 -------------
@@ -23,35 +23,77 @@ Print the required answer for each test case in a new line.
 """
 
 # solution
+def largest_palindrome(upper_bound:int)->tuple:
 
-def largest_palindrome(N):
+    """
+    Function to find the greatest six-digit palindrome below certain upper bound.
 
-    triple = '{}'.format(N)[:3]
+    Parameter
+    ---------
+    upper_bound: int
+        upper bound of the six-digit palindrome
+
+    Return
+    ------
+    int
+    """
+
+    triple = str(upper_bound)[:3]
     str_pal = triple + triple[::-1]
     palindrome = int(str_pal)
-    if palindrome<N:
+
+    if palindrome<upper_bound:
         return palindrome, int(triple)
     else:
-        triple = '{}'.format(N-1000)[:3]
+        triple = str(upper_bound-1000)[:3]
         str_pal = triple + triple[::-1]
         return int(str_pal), int(triple)
 
-def three_digit_divisors(N):
+def three_digit_divisors(N:int)->bool:
 
-    for k in range(102,1000):
-        quot, rem = divmod(N,k)
-        if rem==0 and len(str(quot))==3:
-            return True
+    """
+    Function that determines if certain integer N has a three digit divisor.
+
+    Parameter
+    ---------
+    N: int
+        integer to determine if it has a three digit divisor
+
+    Return
+    ------
+    bool
+    """
+
+    for k in range(100,1000):
+        if k**2 < N:
+            quot, rem = divmod(N,k)
+            if rem==0 and len(str(quot))==3:
+                return True
+        else:
+            continue
     return False
 
-def palindrome_three_digit_factors(N):
+def palindrome_three_digit_factors(upper_bound:int)->int:
 
-    palindrome, triple = largest_palindrome(N)
+    """
+    Function to determine the largest six-digit palindrome below certain upper bound and it is the product of two three-digit numbers.
+
+    Parameter
+    ---------
+    upper_bound: int
+        upper bound for the six digit palindrome
+
+    Return
+    ------
+    int
+    """
+
+    palindrome, triple = largest_palindrome(upper_bound)
 
     while not three_digit_divisors(palindrome):
 
         triple = triple-1
-        temp = '{}'.format(triple)
+        temp = str(triple)
         pal_str = temp + temp[::-1]
         palindrome = int(pal_str)
     return palindrome
